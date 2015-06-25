c78cc523e59ad20b534a151b055c7ba1f2dfb59d
성기평 <sungkipyung@gmail.com>
2015-06-25 15:25:03 +0900

    initial commit

---
+++ NSCacheExample/NSCacheExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NSCacheExample
+//
+//  Created by 성기평 on 2015. 6. 25..
+//  Copyright (c) 2015년 hothead. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
